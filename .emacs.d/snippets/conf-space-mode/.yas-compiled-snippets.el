c0e512858023a33b49e924771eba5afa1978cf79
pollakg <pollakg@student.ethz.ch>
2020-11-22 21:35:23 +0100

    undo moving doom config files to wrong folder

--- .emacs.d/snippets/conf-space-mode/.yas-compiled-snippets.el
+++
@@ -1,9 +0,0 @@
-;;; Compiled snippets and support files for `conf-space-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'conf-space-mode
-                     '(("comment" "#                                      $1\n#============================================================================================\n$0" "comment" nil nil nil "/home/pollakg/.doom.d/snippets/conf-space-mode/comment" nil nil)
-                       ("bcomment" "#============================================================================================\n#                                  $1\n#============================================================================================\n$0" "bcomment" nil nil nil "/home/pollakg/.doom.d/snippets/conf-space-mode/bcomment" nil nil)))
-
-
-;;; Do not edit! File generated at Mon Nov 16 10:09:48 2020
--- .emacs.d/snippets/js-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `js-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'js-mode
-                     '(("doc-file" "/**\n * @fileOverview ${1:file description}\n * @name `(file-name-nondirectory (buffer-file-name))`\n * @author ${2:D-Light GmbH}\n * @license \n */\n$0" "doc-file" nil nil nil "/home/pollakg/.doom.d/snippets/js-mode/doc-file" nil nil)))
-
-
-;;; Do not edit! File generated at Mon Nov 16 10:09:48 2020
