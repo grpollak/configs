5b18e7028686018f88ab2545e7b69a6280ff159d
pollakg <pollakg@student.ethz.ch>
2020-11-22 21:36:36 +0100

    add doom config files

---
+++ .doom.d/snippets/c++-mode/.yas-compiled-snippets.el
@@ -0,0 +1,9 @@
+;;; Compiled snippets and support files for `c++-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'c++-mode
+                     '(("cout" "std::cout << $1 << std::endl;\n$0" "cout" nil nil nil "/home/pollakg/.doom.d/snippets/c++-mode/cout" nil nil)
+                       ("comment" "/*###########################################\n   $1\n*/###########################################\n$0" "comment" nil nil nil "/home/pollakg/.doom.d/snippets/c++-mode/comment" nil nil)))
+
+
+;;; Do not edit! File generated at Mon Nov 16 10:09:48 2020
